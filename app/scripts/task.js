2602cccaaec5d9c291787513fd9beabf2b817dfe
Devon Moubry <devon@moubry.com>
2017-04-04 21:05:49 -0500

    initial styles

--- app/scripts/task.js
+++ app/scripts/task.js
@@ -3,29 +3,31 @@ export default function ( store ) {
   console.log('You are seeing the Task View');
 
   let $html = (`
-    <h1 id=taskHeader>
-      Hi, ${name}! Thank you for logging in. Here are your tasks.
-    </h1>
-    <table>
-      <tr>
-        <th></th>
-        <th>name</th>
-        <th>description</th>
-        <th>complete</th>
-        <th>important</th>
-        <th>due date</th>
-      </tr>
-      <form id="submit_task" action="" method="post">
+    <section class="task-view">
+      <h2>
+        Hi, ${name}! Thank you for logging in. Here are your tasks.
+      </h2>
+      <table>
         <tr>
-          <td><input type="submit" value="add new task"></td>
-          <td><input id="name" placeholder="name" value="connor@example.com"></td>
-          <td><input id="description" placeholder="description" value="check the mail"></td>
-          <td><input id="state" placeholder="state" value="false"></td>
-          <td><input id="important" placeholder="important" value="yes"></td>
-          <td><input id="due_date" placeholder="due_date" value="04/05/2017 5:35pm"></td>
+          <th></th>
+          <th>name</th>
+          <th>description</th>
+          <th>complete</th>
+          <th>important</th>
+          <th>due date</th>
         </tr>
-      </form>
-    </table>
+        <form id="submit_task" action="" method="post">
+          <tr>
+            <td><input type="submit" value="add new task"></td>
+            <td><input id="name" placeholder="name" value="connor@example.com"></td>
+            <td><input id="description" placeholder="description" value="check the mail"></td>
+            <td><input id="state" placeholder="state" value="false"></td>
+            <td><input id="important" placeholder="important" value="yes"></td>
+            <td><input id="due_date" placeholder="due_date" value="04/05/2017 5:35pm"></td>
+          </tr>
+        </form>
+      </table>
+    </section>
   `);
 
   return $html;
